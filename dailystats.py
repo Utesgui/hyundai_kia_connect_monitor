8cebcbb18787edc3875e2c9207a14c2ccf1702c9
Klaus Dandl <76051010+Utesgui@users.noreply.github.com>
2025-08-27 09:46:40 +0000

    Reduce maximum queue length in dailystats.py and summary.py from 990 and 1220 to 900

--- dailystats.py
+++ dailystats.py
@@ -58,7 +58,7 @@ if D:
     set_dbg()
 
 # Initializing a queue for about 30 days
-MAX_QUEUE_LEN = 990
+MAX_QUEUE_LEN = 900
 PRINTED_OUTPUT_QUEUE: deque[str] = deque(maxlen=MAX_QUEUE_LEN)
 
 KEYWORD_LIST = ["help", "sheetupdate", "debug"]
